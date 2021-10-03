47cfb0a5947f89cf2202d4b4f28b1b3e9487f409
Raul Alcantara <ralcanta@mit.edu>
2021-10-03 01:48:46 -0400

    added Redirecter class to handle localhost:5000/<short> urls

---
+++ client/src/components/pages/Redirecter.js
@@ -0,0 +1,36 @@
+import { redirectTo } from "@reach/router";
+import React, { Component } from "react";
+import { getUrlFromDatabase } from "../../utilities";
+
+export default class Redirecter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "",
+      success: true,
+      getMessage: "",
+    };
+  }
+  async componentDidMount() {
+    let response = await getUrlFromDatabase(this.props.short);
+    this.setState(
+      { url: response.original, getMessage: response.msg, success: response.success },
+      () => {
+        if (this.state.success) {
+          window.location.href = this.state.url;
+        }
+      }
+    );
+  }
+  render() {
+    return (
+      <>
+        {this.state.success ? (
+          <div>Redirecting you to {this.state.url}</div>
+        ) : (
+          <div>There was an error: {this.state.getMessage}</div>
+        )}
+      </>
+    );
+  }
+}
