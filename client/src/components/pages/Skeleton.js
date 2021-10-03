b4368d59de466065e210772d76d1adcc5d4e643a
Raul Alcantara <ralcanta@mit.edu>
2021-10-03 01:52:25 -0400

    changed Skeleton class so that it can be the UI to create new Links

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -3,7 +3,7 @@ import GoogleLogin, { GoogleLogout } from "react-google-login";
 
 import "../../utilities.css";
 import "./Skeleton.css";
-
+import { createUrl } from "../../utilities";
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
 
@@ -11,48 +11,61 @@ class Skeleton extends Component {
   constructor(props) {
     super(props);
     // Initialize Default State
-    this.state = {};
+    this.state = {
+      short: "",
+      original: "",
+      probability: 0,
+      isRestarted: true,
+      success: true,
+      createdMessage: "",
+    };
   }
 
   componentDidMount() {
     // remember -- api calls go here!
   }
-
+  onChangeShort = (evt) => {
+    this.setState({ short: evt.target.value });
+  };
+  onChangeOriginal = (evt) => {
+    // this.setState({original: })
+    this.setState({ original: evt.target.value });
+  };
+  onChangeProbability = (evt) => {
+    // this.setState({original: })
+    this.setState({ probability: evt.target.value });
+  };
+  onCreate = async (evt) => {
+    let response = await createUrl(this.state.short, this.state.original, this.state.probability);
+    this.setState({ isRestarted: false, success: response.success, createdMessage: response.msg });
+  };
+  restart = () => {
+    this.setState({
+      short: "",
+      original: "",
+      probability: 0,
+      isRestarted: true,
+      success: true,
+      createdMessage: "",
+    });
+  };
   render() {
     return (
       <>
-        {this.props.userId ? (
-          <GoogleLogout
-            clientId={GOOGLE_CLIENT_ID}
-            buttonText="Logout"
-            onLogoutSuccess={this.props.handleLogout}
-            onFailure={(err) => console.log(err)}
-          />
-        ) : (
-          <GoogleLogin
-            clientId={GOOGLE_CLIENT_ID}
-            buttonText="Login"
-            onSuccess={this.props.handleLogin}
-            onFailure={(err) => console.log(err)}
-          />
+        Input the short version:
+        <textarea value={this.state.short} onChange={this.onChangeShort} />
+        Input your url:
+        <textarea value={this.state.original} onChange={this.onChangeOriginal} />
+        Input your probability:
+        <textarea value={this.state.probability} onChange={this.onChangeProbability} />
+        <button onClick={this.onCreate}> Shorten url!</button>
+        {!this.state.isRestarted && (
+          <div>
+            {this.state.success ? "Yay!" : "Uh oh"}
+            {this.state.createdMessage}
+            <button onClick={this.restart}> Create a new one?</button>
+          </div>
         )}
-        <h1>Good luck on your project :)</h1>
-        <h2> What we provide in this skeleton</h2>
-        <ul>
-          <li>Google Auth (Skeleton.js & auth.js)</li>
-          <li>Socket Infrastructure (client-socket.js & server-socket.js)</li>
-          <li>User Model (auth.js & user.js)</li>
-        </ul>
-        <h2> What you need to change</h2>
-        <ul>
-          <li>Change the font in utilities.css</li>
-          <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js)</li>
-          <li>Change the Server CLIENT_ID for Google Auth (auth.js)</li>
-          <li>Change the Database SRV for Atlas (server.js)</li>
-          <li>Change the Database Name for MongoDB (server.js)</li>
-          <li>Add a favicon to your website at the path client/dist/favicon.ico</li>
-          <li>Update website title in client/dist/index.html</li>
-        </ul>
       </>
     );
   }
